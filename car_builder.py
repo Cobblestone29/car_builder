4eead57419581760d231dde5dc6cc1d99130c159
ZSteck <zsteck@stcharlessd.org>
2018-02-26 13:25:37 -0600

    Removed 350z car type Added sample car type
    Added sample car file
    
    Added performance mods:
    	Brembo brakes
    	Turbocharger
    
    Created a bug where the program will continue to write the new mods
    over and over again. Need to make an exception for mods already in
    the car's file
    
    Solved the "space bug"

--- car_builder.py
+++ car_builder.py
@@ -6,7 +6,7 @@
 
 car = "sports car"
 
-cars_ingame = ["350z"]
+cars_ingame = ["sample"]
 
 def createCar(car_model, car_name, p_mods, v_mods):
 
@@ -29,6 +29,9 @@ def createCar(car_model, car_name, p_mods, v_mods):
     newLine()
     car_file.write("VISUAL MODS")
     newLine()
+    for i in range(30):
+        car_file.write("-")
+    newLine()
     newLine()
     for item in v_mods:
         car_file.write(item)
@@ -43,6 +46,9 @@ def createCar(car_model, car_name, p_mods, v_mods):
     newLine()
     car_file.write("PERFORMANCE MODS")
     newLine()
+    for i in range(30):
+        car_file.write("-")
+    newLine()
     newLine()
     for item in p_mods:
         car_file.write(item)
@@ -54,6 +60,9 @@ def createCar(car_model, car_name, p_mods, v_mods):
 
 def basePrice():
     new_or_current = input("Do you want to modify an [e]xisting car or build a [N]ew one? ")
+
+    performance_mods = []
+    visual_mods = []
     
     if new_or_current.lower() == "e":
         car_name = input("What's the name of the car? ")
@@ -64,9 +73,14 @@ def basePrice():
             basePrice()
         else:
             for line in car_name_file:
+                #print(line)
                 if "MODEL" in line:
-                    car_model = line[11:]
-            pickMods(car_model, car_name)
+                    car_model = line[12:]
+                elif "brakes" in line.lower():
+                    performance_mods.append(line)
+                elif "turbo" in line.lower():
+                    performance_mods.append(line)
+            pickMods(car_model, car_name, performance_mods, visual_mods)
     else:
         car_choice = input("Which car do you want to build? ")
         if car_choice not in cars_ingame:
@@ -91,26 +105,40 @@ def basePrice():
             print(car_choice, "selected.")
             name = input("What would you like to name this car? ")
             createCar(car_choice, name, [], [])
-            pickMods(car_choice, name)
+            pickMods(car_choice, name, [], [])
 
-def pickMods(car_model, car_name):
-    
-    
-    mod_half = input("Do you want to mod [V]isuals or [p]erformance? ")
+def pickMods(car_model, car_name, perf_mods, vis_mods):
+    p_mods = []
+    v_mods = []
+
+    for item in perf_mods:
+        p_mods.append(item)
+    #print(p_mods)
+
+    for item in vis_mods:
+        v_mods.append(item)
+    #print(v_mods)
+        
+    mod_half = input("Do you want to mod visuals or performance? ")
     if mod_half.lower() == "p":
         category_p = input("Which part of the car's performance would you like to modify? ")
-        if category_p == "brakes":
+        if category_p == "brakes" and "Brembo brakes" not in p_mods:
             print("Brakes have been upgraded!")
             p_mods.append("Brembo brakes")
-            print(p_mods)
-            pickMods(car_model, car_name)
-        elif category_p == "Turbocharged engine":
+            #print(p_mods)
+            pickMods(car_model, car_name, p_mods, v_mods)
+        elif category_p == "turbo" and "Turbocharged engine" not in p_mods:
             print("Turbocharged the engine!")
-            p_mods.append("turbo")
-            print(p_mods)
-            pickMods(car_model, car_name)
-        createCar(car_model, car_name, p_mods, v_mods)
-    else:
+            p_mods.append("Turbocharged engine")
+            #print(p_mods)
+            pickMods(car_model, car_name, p_mods, v_mods)
+        else:
+            print("Sorry! That mod isn't in the game yet. ")
+            pickMods(car_model, car_name, p_mods, v_mods)
+    elif mod_half.lower() == "v":
         print("mod visuals here")
+    else:
+        createCar(car_model, car_name, p_mods, v_mods)
+        print("Car file saved.")
 
 basePrice()
